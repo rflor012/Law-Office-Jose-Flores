d84c26477d6e242441cef49f7f9035055bf6960e
Rodrigo Flores <rflor12@gmail.com>
2018-08-28 12:31:02 -0400

    Creating

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -47,7 +47,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Welcome to the Law Offices of Jose L. Flores, Esq.';
 
 
 
